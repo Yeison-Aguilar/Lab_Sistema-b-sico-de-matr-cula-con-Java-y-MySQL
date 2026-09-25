603b8ae03b6b786b5f246af8a93e5530abdb0084
Yeison <yeison.hernandez.aguilar@est.una.ac.cr>
2026-09-25 10:26:39 -0600

    Creacion de proyecto

---
+++ SistemaBasicoDeMatriculaConJavaYMSQL/src/main/java/cr/ac/una/sistemabasicodematriculaconjavaymsql/SistemaBasicoDeMatriculaConJavaYMSQL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cr.ac.una.sistemabasicodematriculaconjavaymsql;
+
+/**
+ *
+ * @author Laboratorio_M
+ */
+public class SistemaBasicoDeMatriculaConJavaYMSQL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
